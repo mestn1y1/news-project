5e7ca4b2aff9dcd439e7d760fefcb2f716bf2206
mestn1y <mestn1y@hotmail.com>
2025-08-05 20:07:35 +0300

    added not found page

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,18 +1,21 @@
 import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
 
 export default function NewsDetailsPage({ params }) {
   const newsSlug = params.slug;
-  const { image, date, content, title } = DUMMY_NEWS.find(
-    (newsItem) => newsItem.slug === newsSlug
-  );
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+
+  if (!newsItem) {
+    notFound();
+  }
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${image}`} />
-        <h1>{title}</h1>
-        <time dateTime={date}>{date}</time>
+        <img src={`/images/news/${newsItem.image}`} />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
       </header>
-      <p>{content}</p>
+      <p>{newsItem.content}</p>
     </article>
   );
 }
