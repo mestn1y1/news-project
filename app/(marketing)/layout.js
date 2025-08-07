fcbac24572fc546d13a2d5f60bee11b63b4aed57
mestn1y <mestn1y@hotmail.com>
2025-08-08 00:05:27 +0300

    using route group and change homePage content

--- app/(marketing)/layout.js
+++ app/(marketing)/layout.js
@@ -1,5 +1,5 @@
 import MainHeader from "@/components/mainHeader";
-import "./globals.css";
+import "../globals.css";
 
 export const metadata = {
   title: "Next.js Page Routing & Rendering",
@@ -9,12 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <div id="page">
-          <MainHeader />
-          {children}
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
