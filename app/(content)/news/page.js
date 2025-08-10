02f0efe047b5b874b232541192e629faa991aeeb
mestn1y <mestn1y@hotmail.com>
2025-08-10 12:26:11 +0300

    added backend and fetch function

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,48 @@
+"use client";
+
 import NewsList from "@/components/news/newsList";
 import { DUMMY_NEWS } from "@/dummy-news";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [news, setNews] = useState();
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const res = await fetch("http://localhost:8080/news");
+
+      if (!res.ok) {
+        setError("Failed to fetch news");
+        setIsLoading(false);
+      }
+
+      const news = await res.json();
+
+      setIsLoading(false);
+      setNews(news);
+    }
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
   return (
     <>
       <h1>News page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
