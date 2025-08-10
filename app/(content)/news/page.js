cdc0f09e8983948970bc75453664eeabe1d805a3
mestn1y <mestn1y@hotmail.com>
2025-08-10 13:31:55 +0300

    delete backend folder and change request link

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -2,7 +2,7 @@ import NewsList from "@/components/news/newsList";
 import { DUMMY_NEWS } from "@/dummy-news";
 
 export default async function NewsPage() {
-  const res = await fetch("http://localhost:8080/news");
+  const res = await fetch("https://backend-news-1-ckvj.onrender.com/news");
   if (!res.ok) {
     throw new Error("Failed to fetch news.");
   }
